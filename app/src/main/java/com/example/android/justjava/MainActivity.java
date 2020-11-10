f2fa9866e3072c6045b93118481d283713fd97f2
NarutoBratishka <katorabian@gmail.com>
2020-11-11 05:14:50 +0700

    add textView, button and operation onClick

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.android.justjava;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+//import com.example.android.justjava.R;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+
+     This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+        displayPrice(1);
+    }
+    /**
+
+     This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
